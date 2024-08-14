4e9dac109d46ea71cf41ee0bc7f9a3fd53beb923
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2024-08-14 15:02:27 -0300

    feat: mapping book, author, publisher and review entities (4)

---
+++ SpringBoot3/aulas/bookstore-api/src/main/java/com/app/bookstore/models/Review.java
@@ -0,0 +1,37 @@
+package com.app.bookstore.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_REVIEW")
+public class Review implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String comment;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
